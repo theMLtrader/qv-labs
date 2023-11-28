2968f91bdf53787cde2948571c00ff26afeedd41
theMLtrader <theMLtrader@users.noreply.github.com>
2023-11-28 09:23:58 +0000

    Add files via upload

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,18 @@
 module.exports = function override(config, env) {
-    // Add a fallback for the crypto module
-    if (!config.resolve.fallback) { config.resolve.fallback = {}; }
+    // Ensure the fallback object exists
+    if (!config.resolve.fallback) {
+        config.resolve.fallback = {};
+    }
+
+    // Existing fallbacks
     config.resolve.fallback.crypto = require.resolve('crypto-browserify');
-    
+    config.resolve.fallback.https = require.resolve('https-browserify');
+    config.resolve.fallback.zlib = require.resolve('browserify-zlib');
+    config.resolve.fallback.url = require.resolve('url/');
+
+    // Add fallbacks for stream and http modules
+    config.resolve.fallback.stream = require.resolve('stream-browserify');
+    config.resolve.fallback.http = require.resolve('stream-http');
+
     return config;
 }
